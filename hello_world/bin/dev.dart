2a981ee0d4996ec77e0044212cc1765d528f9666
Marco Del Angel <marco-del-angel@hotmial.com>
2022-09-20 11:43:14 -0500

    commit

--- hello_world/bin/dev.dart
+++ hello_world/bin/dev.dart
@@ -1,6 +1,15 @@
-
-void main(List<String> arguments){
-  print('dev mode');
-  print(arguments);
-
+void main(List<String> arguments) {
+  List<int> numbers = [];
+  List<int> numbers2 = [9, 10];
+  print(numbers);
+  numbers.add(4);
+  print(numbers);
+  numbers.addAll(numbers2);
+  print(numbers);
+  numbers.removeAt(0);
+  print(numbers);
+  numbers.removeLast();
+  print(numbers);
+  numbers.clear();
+  print(numbers);
 }
